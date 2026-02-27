e36d460784ac4773e5dd29b9941ff28d9689fb41
Jesse Gallagher <jesse@secondfoundation.org>
2026-02-27 11:29:44 -0500

    Add NotesSummaryItem and related entities

---
+++ xpages-build-stubs/com.ibm.domino.napi/src/main/java/com/ibm/designer/domino/napi/NotesItem.java
@@ -0,0 +1,7 @@
+package com.ibm.designer.domino.napi;
+
+public class NotesItem extends NotesObject implements NotesConstants {
+    public String getValueAsString() throws NotesAPIException {
+        return null;
+    }
+}
