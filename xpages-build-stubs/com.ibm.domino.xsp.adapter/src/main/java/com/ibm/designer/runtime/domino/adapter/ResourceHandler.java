fd4e3f98748e9e7db28553a1d25f8f423d3284a6
Jesse Gallagher <jesse@secondfoundation.org>
2026-07-16 11:06:21 -0400

    Add ResourceHandler class and remaining methods from XSPErrorPage

---
+++ xpages-build-stubs/com.ibm.domino.xsp.adapter/src/main/java/com/ibm/designer/runtime/domino/adapter/ResourceHandler.java
@@ -0,0 +1,11 @@
+package com.ibm.designer.runtime.domino.adapter;
+
+public class ResourceHandler {
+	public static String getString(String key) {
+		return null;
+	}
+	
+	public static String getLoggingString(String key) {
+		return null;
+	}
+}
--- xpages-build-stubs/com.ibm.domino.xsp.adapter/src/main/java/com/ibm/designer/runtime/domino/adapter/util/XSPErrorPage.java
+++ xpages-build-stubs/com.ibm.domino.xsp.adapter/src/main/java/com/ibm/designer/runtime/domino/adapter/util/XSPErrorPage.java
@@ -4,12 +4,28 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
 
 public class XSPErrorPage {
+	public static PrintWriter getPrintWriterForError(ServletResponse paramServletResponse) throws ServletException {
+		return null;
+	}
 	public static void handleException(PrintWriter var0, Throwable var1, String var2, boolean var3) throws IOException, ServletException {
 		
 	}
 	public static void handlePageNotFound(PrintWriter var0, String var1, Exception var2, String var3, boolean var4) throws IOException {
+		
+	}
+	public static void writeErrorPageHeader(PrintWriter paramPrintWriter, String paramString1, String paramString2, boolean paramBoolean) throws IOException {
+		
+	}
+	public static void writeExceptionMessage(PrintWriter paramPrintWriter, Throwable paramThrowable) {
+		
+	}
+	public static void writeExceptionTrace(PrintWriter paramPrintWriter, Throwable paramThrowable, boolean paramBoolean) {
+		
+	}
+	public static void writeErrorPageFooter(PrintWriter paramPrintWriter) throws IOException {
 		
 	}
 }
